b6b324c2399e9e69cf4622e4f4c639ae97e4612c
kabermedhasni <kabermedhasni653@gmail.com>
2025-11-14 16:52:02 +0000

    small updates

--- src/views/timetable_view.php
+++ src/views/timetable_view.php
@@ -138,12 +138,14 @@ $headerBg = ($role === 'student') ? 'blue' : 'purple';
                     Retour à la Sélection
                 </a>
                 <?php endif; ?>
+                <?php if (!$isProfessorDebug): ?>
                 <a href="logout.php" class="btn">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                     </svg>
                     Déconnexion
                 </a>
+                <?php endif; ?>
             </div>
         </div>
 
