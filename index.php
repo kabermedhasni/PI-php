75fca504b738c86f26e78ba67c0145457c6d8240
kabermedhasni <kabermedhasni653@gmail.com>
2025-11-16 20:15:46 +0000

    updates for the css

--- index.php
+++ index.php
@@ -150,7 +150,7 @@ $headerBg = ($role === 'student') ? 'blue' : 'purple';
             <h1 class="page-title"><?php echo $pageTitle; ?></h1>
             <div class="actions">
                 <?php if ($isProfessorDebug): ?>
-                <a href="admin/professor.php" class="btn">
+                <a href="admin/professor.php" class="btn" style=" background-color: rgba(255, 255, 255, 0.2) ;border: 1px solid rgba(255, 255, 255, 0.3) ;border-radius: 0.375rem ;">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                     </svg>
@@ -158,7 +158,7 @@ $headerBg = ($role === 'student') ? 'blue' : 'purple';
                 </a>
                 <?php endif; ?>
                 <?php if (!$isProfessorDebug): ?>
-                <a href="core/logout.php" class="btn">
+                <a href="core/logout.php" class="btn" style=" background-color: rgba(255, 255, 255, 0.2) ;border: 1px solid rgba(255, 255, 255, 0.3) ;border-radius: 0.375rem ;">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                     </svg>
